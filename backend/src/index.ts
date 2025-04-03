60d74d990ef93aa788d13eb601b016139df5131f
KALYAN GUPTA <kalyangupta2002@gmail.com>
2025-04-03 19:21:17 +0530

    kcuh

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
-
+import prisma from "./config/db"
 dotenv.config()
 
 const app = express()
@@ -11,6 +11,33 @@ app.use(express.json())
 app.get("/", (req, res) => {
   res.send("Hello, Express with TypeScript!")
 })
+app.post("/api/contact", async (req, res) => {
+
+  try{
+    const { name, email, subject, message } = req.body;
+
+    if(!name || !email || !subject || !message) {
+      res.status(400).json({ error: "All fields are required." });
+      return;
+    }
+    const contactForm = await prisma.contact.create({
+      data: {
+        name,
+        email,
+        subject,
+        message,
+      },
+    })
+    if (!contactForm) {
+      res.status(500).json({ error: "Failed to submit the form." });
+      return;
+    }
+    res.status(200).json({ message: "Form submitted successfully!" });
+  } catch (error) {
+    console.error("Error submitting form:", error);
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
